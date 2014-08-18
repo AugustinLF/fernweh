4f9a044b47bc5b5e451215405356701b57b12fa3
AugustinLF <augustin.public@gmail.com>
2014-08-18 02:05:04 +0200

    working non-tested version with login/logout

--- js/angular.app.js
+++ js/angular.app.js
@@ -1,230 +1,296 @@
 (function() {
-	var app = angular.module('app', ['iso.directives']);
+  var app = angular.module('app', ['iso.directives', 'ui.router']);/*.run('$stateProvider', '$urlRouterProvider', '$state', function($stateProvider, $urlRouterProvider, $state) {
 
-	//headroom
-	app.directive('headroom', function() {
-		return {
-			restrict: 'EA',
-			scope: {
-				tolerance: '=',
-				offset: '=',
-				classes: '='
-			},
-			link: function(scope, element) {
-				var options = {};
-				angular.forEach(Headroom.options, function(value, key) {
-					options[key] = scope[key] || Headroom.options[key];
-				});
-				var headroom = new Headroom(element[0], options);
-				headroom.init();
-				scope.$on('destroy', function() {
-					headroom.destroy();
-				});
-			}
-		};
-	});
+  });*/
 
-	app.controller('gridController', function() {
-		this.photos = photos;
-	});
+  Parse.initialize("ePIyiLv520OUiP4YYPdUFwue7JeVsWsxsSUvvUpc", "xAqLZYUadKWeZrLf2x5aF4RtJyH086R6I1cbbMjk");
 
-	app.directive('mainGrid', function() {
-		return {
-			restrict: 'E',
-			templateUrl: 'main_grid.html'
-		};
-	});
+  app.config(['$stateProvider', function($stateProvider) {
+    // Set up the states
+    $stateProvider
+      .state('connected', {
+        templateUrl: "gallery.html"
+      })
+      .state('notConnected', {
+        templateUrl: "signup.html"
+      });
+    }]);
 
-	app.controller('itemController', function() {
+  //headroom
+  app.directive('headroom', function() {
+    return {
+      restrict: 'EA',
+      scope: {
+        tolerance: '=',
+        offset: '=',
+        classes: '='
+      },
+      link: function(scope, element) {
+        var options = {};
+        angular.forEach(Headroom.options, function(value, key) {
+          options[key] = scope[key] || Headroom.options[key];
+        });
+        var headroom = new Headroom(element[0], options);
+        headroom.init();
+        scope.$on('destroy', function() {
+          headroom.destroy();
+        });
+      }
+    };
+  });
 
-		this.isSrc = 'load';
-		this.setSrc = function(setSrc) {
-			this.isSrc = setSrc;
-			/*$('#itemContainer').isotope('layout');*/
-		}
-	});
+  app.controller('indexController', function($state) {
 
-	app.controller('navController', function() {
-	});
+      if (Parse.User.current()) {
+        $state.go('connected');
+      } else {
+        $state.go('notConnected');
+      }
+
+      this.logOut = function() {
+        Parse.User.logOut();
+        $state.go('notConnected');
+      };
+  });
+
+  app.controller('signInController', function($state) {
+    this.user = {};
+    this.signIn = function() {
+      Parse.User.logIn(this.user.email, this.user.password, {
+        success: function(user) {
+          $state.go('connected');
+        },
+        error: function(user, error) {
+          // The login failed. Check error to see why.
+          alert("Error: " + error.code + " " + error.message);
+        }
+      });
+    };
+  });
+
+  app.controller('registerController', function() {
+    this.user = {};
+    this.addUser = function() {
+      var user = new Parse.User();
+      user.set('username', this.user.email);
+      user.set('email', this.user.email);
+      user.set('password', this.user.password);
+
+      user.signUp(null, {
+        success: function(user) {
+          // Hooray! Let them use the app now.
+        },
+        error: function(user, error) {
+          // Show the error message somewhere and let the user try again.
+          alert("Error: " + error.code + " " + error.message);
+        }
+      });
+      this.user = {};
+    };
+  });
+
+  app.controller('gridController', function() {
+    this.photos = photos;
+  });
+
+  app.directive('mainGrid', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'main_grid.html'
+    };
+  });
+
+  app.controller('itemController', function() {
+
+    this.isSrc = 'load';
+    this.setSrc = function(setSrc) {
+      this.isSrc = setSrc;
+      /*$('#itemContainer').isotope('layout');*/
+    }
+  });
+
+  app.controller('navController', function() {
+
+  });
 
 
-	// will be send via AJAX
-	var photos = [
-	{
-		id: 1,
-		country: 'Denmark',
-		month: 'May',
-		year: 2014,
-		user: 'Thomas',
-		img: {
-			small: {
-				src: "img/photos/300/1.jpg",
-				height: 400,
-				width: 300
-			},
-			big: {
-				src: "img/photos/600/1.jpg",
-				height: 375,
-				width: 600
-			},
-			load: {
-				src: "img/anchor3_35.png",
-				height: 400,
-				width: 300
-			}
-		},
-		history: {
-			liked: 1,
-			followed: 1,
-			shared: "yes"
-		}
-	},
-	{
-		id: 2,
-		country: 'Italy',
-		month: 'April',
-		year: 2000,
-		user: 'John',
-		img: {
-			small: {
-				src: "img/photos/300/2.jpg",
-				height: 400,
-				width: 300
-			},
-			big: {
-				src: "img/photos/600/2.jpg",
-				height: 375,
-				width: 600
-			},
-			load: {
-				src: "img/anchor3_35.png",
-				height: 400,
-				width: 300
-			}
-		},
-		history: {
-			liked: 1,
-			followed: 1,
-			shared: "yes"
-		}
-	},
-	{
-		id: 3,
-		country: 'Spain',
-		month: 'July',
-		year: 2025,
-		user: 'james',
-		img: {
-			small: {
-				src: "img/photos/300/3.jpg",
-				height: 225,
-				width: 300
-			},
-			big: {
-				src: "img/photos/600/3.jpg",
-				height: 375,
-				width: 600
-			},
-			load: {
-				src: "img/anchor3_35.png",
-				height: 225,
-				width: 300
-			}
-		},
-		history: {
-			liked: 1,
-			followed: 1,
-			shared: "yes"
-		}
-	},
-	{
-		id: 1,
-		country: 'Denmark',
-		month: 'May',
-		year: 2014,
-		user: 'Thomas',
-		img: {
-			small: {
-				src: "img/photos/300/1.jpg",
-				height: 400,
-				width: 300
-			},
-			big: {
-				src: "img/photos/600/1.jpg",
-				height: 375,
-				width: 600
-			},
-			load: {
-				src: "img/anchor3_35.png",
-				height: 400,
-				width: 300
-			}
-		},
-		history: {
-			liked: 1,
-			followed: 1,
-			shared: "yes"
-		}
-	},
-	{
-		id: 2,
-		country: 'Italy',
-		month: 'April',
-		year: 2000,
-		user: 'John',
-		img: {
-			small: {
-				src: "img/photos/300/2.jpg",
-				height: 400,
-				width: 300
-			},
-			big: {
-				src: "img/photos/600/2.jpg",
-				height: 375,
-				width: 600
-			},
-			load: {
-				src: "img/anchor3_35.png",
-				height: 400,
-				width: 300
-			}
-		},
-		history: {
-			liked: 1,
-			followed: 1,
-			shared: "yes"
-		}
-	},
-	{
-		id: 3,
-		country: 'Spain',
-		month: 'July',
-		year: 2025,
-		user: 'james',
-		img: {
-			small: {
-				src: "img/photos/300/3.jpg",
-				height: 225,
-				width: 300
-			},
-			big: {
-				src: "img/photos/600/3.jpg",
-				height: 375,
-				width: 600
-			},
-			load: {
-				src: "img/anchor3_35.png",
-				height: 225,
-				width: 300
-			}
-		},
-		history: {
-			liked: 1,
-			followed: 1,
-			shared: "yes"
-		}
-	}
-	];
+  // will be send via AJAX
+  var photos = [
+    {
+      id: 1,
+      country: 'Denmark',
+      month: 'May',
+      year: 2014,
+      user: 'Thomas',
+      img: {
+        small: {
+          src: "img/photos/300/1.jpg",
+          height: 400,
+          width: 300
+        },
+        big: {
+          src: "img/photos/600/1.jpg",
+          height: 375,
+          width: 600
+        },
+        load: {
+          src: "img/anchor3_35.png",
+          height: 400,
+          width: 300
+        }
+      },
+      history: {
+        liked: 1,
+        followed: 1,
+        shared: "yes"
+      }
+    },
+    {
+      id: 2,
+      country: 'Italy',
+      month: 'April',
+      year: 2000,
+      user: 'John',
+      img: {
+        small: {
+          src: "img/photos/300/2.jpg",
+          height: 400,
+          width: 300
+        },
+        big: {
+          src: "img/photos/600/2.jpg",
+          height: 375,
+          width: 600
+        },
+        load: {
+          src: "img/anchor3_35.png",
+          height: 400,
+          width: 300
+        }
+      },
+      history: {
+        liked: 1,
+        followed: 1,
+        shared: "yes"
+      }
+    },
+    {
+      id: 3,
+      country: 'Spain',
+      month: 'July',
+      year: 2025,
+      user: 'james',
+      img: {
+        small: {
+          src: "img/photos/300/3.jpg",
+          height: 225,
+          width: 300
+        },
+        big: {
+          src: "img/photos/600/3.jpg",
+          height: 375,
+          width: 600
+        },
+        load: {
+          src: "img/anchor3_35.png",
+          height: 225,
+          width: 300
+        }
+      },
+      history: {
+        liked: 1,
+        followed: 1,
+        shared: "yes"
+      }
+    },
+    {
+      id: 1,
+      country: 'Denmark',
+      month: 'May',
+      year: 2014,
+      user: 'Thomas',
+      img: {
+        small: {
+          src: "img/photos/300/1.jpg",
+          height: 400,
+          width: 300
+        },
+        big: {
+          src: "img/photos/600/1.jpg",
+          height: 375,
+          width: 600
+        },
+        load: {
+          src: "img/anchor3_35.png",
+          height: 400,
+          width: 300
+        }
+      },
+      history: {
+        liked: 1,
+        followed: 1,
+        shared: "yes"
+      }
+    },
+    {
+      id: 2,
+      country: 'Italy',
+      month: 'April',
+      year: 2000,
+      user: 'John',
+      img: {
+        small: {
+          src: "img/photos/300/2.jpg",
+          height: 400,
+          width: 300
+        },
+        big: {
+          src: "img/photos/600/2.jpg",
+          height: 375,
+          width: 600
+        },
+        load: {
+          src: "img/anchor3_35.png",
+          height: 400,
+          width: 300
+        }
+      },
+      history: {
+        liked: 1,
+        followed: 1,
+        shared: "yes"
+      }
+    },
+    {
+      id: 3,
+      country: 'Spain',
+      month: 'July',
+      year: 2025,
+      user: 'james',
+      img: {
+        small: {
+          src: "img/photos/300/3.jpg",
+          height: 225,
+          width: 300
+        },
+        big: {
+          src: "img/photos/600/3.jpg",
+          height: 375,
+          width: 600
+        },
+        load: {
+          src: "img/anchor3_35.png",
+          height: 225,
+          width: 300
+        }
+      },
+      history: {
+        liked: 1,
+        followed: 1,
+        shared: "yes"
+      }
+    }
+  ];
 
 
 })();
