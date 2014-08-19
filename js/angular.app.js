3f70703901a3bd017db4839a60f0c06695cb16a5
tdotom <tdotom@gmail.com>
2014-08-19 20:00:28 +0200

    no message

--- js/angular.app.js
+++ js/angular.app.js
@@ -1,6 +1,10 @@
 (function() {
 	var app = angular.module('app', ['iso.directives']);
 
+	//bodytag
+	app.controller('siteController', function($scope) {
+	});
+
 	//headroom
 	app.directive('headroom', function() {
 		return {
@@ -26,6 +30,13 @@
 
 	app.controller('gridController', function() {
 		this.photos = photos;
+
+		this.loadMore = function() {
+			morephotos.forEach(function(morephoto) {
+				photos.push(morephoto);
+			}); 		/* <=> photos.push.apply(photos,morephotos);*/
+		}
+		/* needs to ajax request new morephotos array! */
 	});
 
 	app.directive('mainGrid', function() {
@@ -38,9 +49,17 @@
 	app.controller('itemController', function() {
 
 		this.isSrc = 'load';
+
+
+		/* If imgSRC is changed use this (ng-click="item.setSrc('big')" )
+
 		this.setSrc = function(setSrc) {
 			this.isSrc = setSrc;
-			/*$('#itemContainer').isotope('layout');*/
+		}*/
+
+
+		this.followUser = function(status, username) {
+			this.isSrc = setSrc;
 		}
 	});
 
@@ -48,183 +67,32 @@
 	});
 
 
-	// will be send via AJAX
+
+
 	var photos = [
-	{
-		id: 1,
-		country: 'Denmark',
-		month: 'May',
-		year: 2014,
-		user: 'Thomas',
-		img: {
-			small: {
-				src: "img/photos/300/1.jpg",
-				height: 400,
-				width: 300
-			},
-			big: {
-				src: "img/photos/600/1.jpg",
-				height: 375,
-				width: 600
-			},
-			load: {
-				src: "img/anchor3_35.png",
-				height: 400,
-				width: 300
-			}
-		},
-		history: {
-			liked: 1,
-			followed: 1,
-			shared: "yes"
-		}
-	},
-	{
-		id: 2,
-		country: 'Italy',
-		month: 'April',
-		year: 2000,
-		user: 'John',
-		img: {
-			small: {
-				src: "img/photos/300/2.jpg",
-				height: 400,
-				width: 300
-			},
-			big: {
-				src: "img/photos/600/2.jpg",
-				height: 375,
-				width: 600
-			},
-			load: {
-				src: "img/anchor3_35.png",
-				height: 400,
-				width: 300
-			}
-		},
-		history: {
-			liked: 1,
-			followed: 1,
-			shared: "yes"
-		}
-	},
-	{
-		id: 3,
-		country: 'Spain',
-		month: 'July',
-		year: 2025,
-		user: 'james',
-		img: {
-			small: {
-				src: "img/photos/300/3.jpg",
-				height: 225,
-				width: 300
-			},
-			big: {
-				src: "img/photos/600/3.jpg",
-				height: 375,
-				width: 600
-			},
-			load: {
-				src: "img/anchor3_35.png",
-				height: 225,
-				width: 300
-			}
-		},
-		history: {
-			liked: 1,
-			followed: 1,
-			shared: "yes"
-		}
-	},
-	{
-		id: 1,
-		country: 'Denmark',
-		month: 'May',
-		year: 2014,
-		user: 'Thomas',
-		img: {
-			small: {
-				src: "img/photos/300/1.jpg",
-				height: 400,
-				width: 300
-			},
-			big: {
-				src: "img/photos/600/1.jpg",
-				height: 375,
-				width: 600
-			},
-			load: {
-				src: "img/anchor3_35.png",
-				height: 400,
-				width: 300
-			}
-		},
-		history: {
-			liked: 1,
-			followed: 1,
-			shared: "yes"
-		}
-	},
-	{
-		id: 2,
-		country: 'Italy',
-		month: 'April',
-		year: 2000,
-		user: 'John',
-		img: {
-			small: {
-				src: "img/photos/300/2.jpg",
-				height: 400,
-				width: 300
-			},
-			big: {
-				src: "img/photos/600/2.jpg",
-				height: 375,
-				width: 600
-			},
-			load: {
-				src: "img/anchor3_35.png",
-				height: 400,
-				width: 300
-			}
-		},
-		history: {
-			liked: 1,
-			followed: 1,
-			shared: "yes"
-		}
-	},
-	{
-		id: 3,
-		country: 'Spain',
-		month: 'July',
-		year: 2025,
-		user: 'james',
-		img: {
-			small: {
-				src: "img/photos/300/3.jpg",
-				height: 225,
-				width: 300
-			},
-			big: {
-				src: "img/photos/600/3.jpg",
-				height: 375,
-				width: 600
-			},
-			load: {
-				src: "img/anchor3_35.png",
-				height: 225,
-				width: 300
-			}
-		},
-		history: {
-			liked: 1,
-			followed: 1,
-			shared: "yes"
-		}
-	}
+		{"id":"1","country":'Denmark',"month":"May","year":"2003","user":"Thomas","img":{"load": {"src":"img/photos/300/1.jpg","height":"400","width":"300"} } },
+		{"id":"2","country":'Italy',"month":"April","year":"2008","user":"Thomas","img":{"load": {"src":"img/photos/300/2.jpg","height":"400","width":"300"} } },
+		{"id":"3","country":'Germany',"month":"May","year":"2014","user":"Thomas","img":{"load": {"src":"img/photos/300/3.jpg","height":"225","width":"300"} } },
+		{"id":"4","country":'USA',"month":"September","year":"2014","user":"Thomas","img":{"load": {"src":"img/photos/300/2.jpg","height":"400","width":"300"} } },
+		{"id":"5","country":'Denmark',"month":"November","year":"2009","user":"Thomas","img":{"load": {"src":"img/photos/300/3.jpg","height":"225","width":"300"} } }
 	];
+
+	var morephotos = [
+		{"id":"6","country":'Denmark',"month":"May","year":"2003","user":"Thomas","img":{"load": {"src":"img/photos/300/1.jpg","height":"400","width":"300"} } },
+		{"id":"7","country":'Italy',"month":"April","year":"2008","user":"Thomas","img":{"load": {"src":"img/photos/300/2.jpg","height":"400","width":"300"} } },
+		{"id":"8","country":'Germany',"month":"May","year":"2014","user":"Thomas","img":{"load": {"src":"img/photos/300/3.jpg","height":"225","width":"300"} } },
+		{"id":"9","country":'USA',"month":"September","year":"2014","user":"Thomas","img":{"load": {"src":"img/photos/300/2.jpg","height":"400","width":"300"} } },
+		{"id":"10","country":'Denmark',"month":"November","year":"2009","user":"Thomas","img":{"load": {"src":"img/photos/300/3.jpg","height":"225","width":"300"} } },
+		{"id":"11","country":'Italy',"month":"April","year":"2008","user":"Thomas","img":{"load": {"src":"img/photos/300/2.jpg","height":"400","width":"300"} } },
+		{"id":"12","country":'Germany',"month":"May","year":"2014","user":"Thomas","img":{"load": {"src":"img/photos/300/3.jpg","height":"225","width":"300"} } },
+		{"id":"13","country":'USA',"month":"September","year":"2014","user":"Thomas","img":{"load": {"src":"img/photos/300/2.jpg","height":"400","width":"300"} } },
+		{"id":"14","country":'Denmark',"month":"November","year":"2009","user":"Thomas","img":{"load": {"src":"img/photos/300/3.jpg","height":"225","width":"300"} } },
+		{"id":"15","country":'Italy',"month":"April","year":"2008","user":"Thomas","img":{"load": {"src":"img/photos/300/2.jpg","height":"400","width":"300"} } },
+		{"id":"16","country":'Germany',"month":"May","year":"2014","user":"Thomas","img":{"load": {"src":"img/photos/300/3.jpg","height":"225","width":"300"} } },
+		{"id":"17","country":'USA',"month":"September","year":"2014","user":"Thomas","img":{"load": {"src":"img/photos/300/2.jpg","height":"400","width":"300"} } },
+		{"id":"18","country":'Denmark',"month":"November","year":"2009","user":"Thomas","img":{"load": {"src":"img/photos/300/3.jpg","height":"225","width":"300"} } }
+	];
+
 
 
 })();
