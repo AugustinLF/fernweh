1830871bebcaee62d62470a29877778f810a0440
AugustinLF <augustin.public@gmail.com>
2014-08-21 01:05:01 +0200

    fixed merge conflicts

--- app/js/angular.app.js
+++ app/js/angular.app.js
@@ -18,24 +18,6 @@ angular.module('photoApp', [
 
   }]);
 
-  app.directive('formAutofillFix', function() {
-    return function(scope, elem, attrs) {
-      // Fixes Chrome bug: https://groups.google.com/forum/#!topic/angular/6NlucSskQjY
-      elem.prop('method', 'POST');
-   
-      // Fix autofill issues where Angular doesn't know about autofilled inputs
-      if(attrs.ngSubmit) {
-        setTimeout(function() {
-          elem.unbind('submit').submit(function(e) {
-            e.preventDefault();
-            elem.find('input, textarea, select').trigger('input').trigger('change').trigger('keydown');
-            scope.$apply(attrs.ngSubmit);
-          });
-        }, 0);
-      }
-    };
-  });
-
   var photos = [
     {"id":"1","country":'Denmark',"month":"May","year":"2003","user":"Thomas","img":{"load": {"src":"img/photos/300/1.jpg","height":"400","width":"300"} } },
     {"id":"2","country":'Italy',"month":"April","year":"2008","user":"Thomas","img":{"load": {"src":"img/photos/300/2.jpg","height":"400","width":"300"} } },
--- app/js/directives.js
+++ app/js/directives.js
@@ -27,4 +27,21 @@ angular.module('photoAppDirectives', ['iso.directives'])
       restrict: 'E',
       templateUrl: 'partials/main_grid.html'
     };
+  })
+  .directive('formAutofillFix', function() {
+    return function(scope, elem, attrs) {
+      // Fixes Chrome bug: https://groups.google.com/forum/#!topic/angular/6NlucSskQjY
+      elem.prop('method', 'POST');
+   
+      // Fix autofill issues where Angular doesn't know about autofilled inputs
+      if(attrs.ngSubmit) {
+        setTimeout(function() {
+          elem.unbind('submit').submit(function(e) {
+            e.preventDefault();
+            elem.find('input, textarea, select').trigger('input').trigger('change').trigger('keydown');
+            scope.$apply(attrs.ngSubmit);
+          });
+        }, 0);
+      }
+    };
   });
