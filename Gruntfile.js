7a4ca141f1c73e3c2ee114becbf352cdea2a90b2
Thomas Michaeler <tdotom@gmail.com>
2014-12-07 23:15:50 -0200

    - complete CSS overhaul - SASS implementation - redesign of cards, login, etc. (HTML and CSS)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,28 @@
 /* jshint node: true*/
 
+'use strict';
+
 module.exports = function( grunt ) {
   // Project configuration.
   grunt.initConfig({
     // This line makes your node configurations available for use
     pkg: grunt.file.readJSON('package.json'),
+
+    'http-server': {
+        // the server root directory
+        root: 'app/index.html',
+
+        host: 'localhost',
+        port: 8080,
+
+        cache: 1,
+        showDir : true,
+        autoIndex: true,
+        defaultExt: "html",
+
+        // run in parallel with other tasks
+        runInBackground: false
+    },
 
     jshint: {
       // The paths tell JSHint which files to validate
@@ -29,35 +47,63 @@ module.exports = function( grunt ) {
       }
     },
 
-    'sass': {
+    sass: {
       dist: {
         files: {
-          'app/css/style.css': 'app/css-sources/style.scss'
+          'app/css/main.css': 'app/css-sources/main.scss'
         }
       }
     },
+  // for the svg to sprite sheet plugin
+    svgstore: {
+      options: {
+        prefix: 'icon-', // This will prefix each ID
+        cleanup: ['fill', 'style'],
+        svg: {
+          xmlns: 'http://www.w3.org/2000/svg',
+          style: 'display: none;'
+        }
+      },
+      default: {
+        files: {
+          'app/img/dest.svg': ['app/img/svgs/*.svg'],
+        },
+      }
+    },
 
-    'http-server': {
-        // the server root directory
-        root: 'app/index.html',
-
-        host: 'localhost',
-        port: 8080,
-
-        cache: 1,
-        showDir : true,
-        autoIndex: true,
-        defaultExt: "html",
-
-        // run in parallel with other tasks
-        runInBackground: false
+    watch: {
+      js: {
+        files: ['app/js/*.js'],
+        tasks: ['jshint'],
+        options: {
+          spawn: false,
+        },
+      },
+      svg: {
+        files: ['app/img/svgs/*.svg'],
+        tasks: ['svgstore'],
+        options: {
+          spawn: false
+        }
+      },
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass'],
+        options: {
+          spawn: false
+        }
+      }
     }
+
   });
 
   // Each plugin must be loaded following this pattern
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-http-server');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-svgstore');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask( 'default', ['jshint'] );
+  grunt.registerTask('default', 'http-server' );
+  grunt.registerTask('dev', ['watch'] );
 };
